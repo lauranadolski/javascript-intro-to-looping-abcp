53d793e24a7dff68a70db229ce5233754da56233
Laura Nadolski <laura.nadolski@gmail.com>
2018-01-16 20:13:47 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,10 +2,10 @@ function forLoop (array) {
   
   for (var i = 0; i < 25; i++) {
     if (i>1){
-      array.shift("I am ${i} strange loops.");
+      array.push("I am ${i} strange loops.");
     }
     else {
-      array.shift("I am 1 strange loop.");
+      array.push("I am 1 strange loop.");
     }}
   return array;
 }
